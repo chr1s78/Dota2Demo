a1dc8c635f9333ebf6714469ff1d01fc29864928
chr1s78 <ayahirano2014@126.com>
2021-09-22 12:34:02 +0800

    Initial Commit

---
+++ Dota2Demo/Dota2DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2DemoApp.swift
+//  Dota2Demo
+//
+//  Created by Chr1s on 2021/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Dota2DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
